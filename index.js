b8c83ba936bd3130844de4e00b26f9723c0114ee
Muhammed Sihebi <muhammednurpro@gmail.com>
2022-04-09 22:42:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName){
+    const messages = []; 
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        messages.push(`Thank you, ${name}, for the wonderful ${eventName} gift!`);  
+    } 
+    return messages; 
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--; 
+    }
+}
